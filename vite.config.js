50bdc797ddb7be13e37e17f3e5458ebe1647685d
Raveena Dhakar <ravinadhakar351@gmail.com>
2025-08-31 18:56:44 +0530

    Done with Jan Sahayak project of Murf CC4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This is from the previous fix
+    port: 5173,
+    allowedHosts: [
+      'jan-sahayak.onrender.com', // Add your domain here
+    ],
+  },
 })
